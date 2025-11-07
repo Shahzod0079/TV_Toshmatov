af82c7e8af16fa867a31b570752740f9ead5a556
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-07 21:44:39 +0500

    Реализация громкости. Тошматов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace TV_Toshmatov
 {
@@ -7,21 +8,55 @@ namespace TV_Toshmatov
     {
         Classes.TV TV = new Classes.TV();
 
+        private DispatcherTimer autoSwitchTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+
             VideoPlayer.Source = new Uri(TV.Channels[TV.ActiveChannel].Src);
             NameChannel.Content = TV.Channels[TV.ActiveChannel].Name;
+
             VideoPlayer.Play();
+
+            InitializeAutoSwitchTimer();
+        }
+
+        private void InitializeAutoSwitchTimer()
+        {
+            autoSwitchTimer = new DispatcherTimer();
+
+            autoSwitchTimer.Interval = TimeSpan.FromSeconds(10);
+            autoSwitchTimer.Tick += AutoSwitchTimer_Tick;
+  
+            autoSwitchTimer.Start();
+        }
+
+        private void AutoSwitchTimer_Tick(object sender, EventArgs e)
+        {
+            TV.NextChannel(VideoPlayer, NameChannel);
         }
 
         private void BackChannel(object sender, RoutedEventArgs e)
         {
             TV.BackChannel(VideoPlayer, NameChannel);
         }
+
         private void NextChannel(object sender, RoutedEventArgs e)
         {
             TV.NextChannel(VideoPlayer, NameChannel);
+        }
+
+        private void ToggleAutoSwitch(object sender, RoutedEventArgs e)
+        {
+            if (autoSwitchTimer.IsEnabled)
+            {
+                autoSwitchTimer.Stop();
+            }
+            else
+            {
+                autoSwitchTimer.Start();
+            }
         }
     }
 }
